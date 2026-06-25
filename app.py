511bdf224200c1222099a6100bf2109bad547312
Corvidae021 <NoctisLumen1221@gmail.com>
2026-06-25 14:08:00 +1200

    Initial framework

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flowist!"
+    return render_template("index.html")
+
+@app.route("/tasks")
+def tasks():
+    return "Tasks Page"
+
+@app.route("/Cat")
+def about():
+    return "About Cat"
 
 if __name__ == "__main__":
     app.run(debug=True)
